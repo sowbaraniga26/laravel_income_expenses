aa350218862aa0f3503481b843702ae5856cf0a3
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-01-10 18:54:44 +0530

    TransactionResource All, Income, Expense Tab filter added

--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -6,6 +6,13 @@ use App\Filament\Resources\TransactionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+
+use App\Enums\TransactionType;
+
+use App\Models\Transaction;
+
 class ListTransactions extends ListRecords
 {
     protected static string $resource = TransactionResource::class;
@@ -14,6 +21,22 @@ class ListTransactions extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+
+            'Income' => Tab::make()
+                ->badge(Transaction::query()->where('type', TransactionType::INCOME)->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TransactionType::INCOME)),
+                
+            'Expense' => Tab::make()
+                ->badge(Transaction::query()->where('type', TransactionType::EXPENSE)->count())
+                ->badgeColor('danger')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TransactionType::EXPENSE)),
         ];
     }
 }
